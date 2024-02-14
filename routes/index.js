7ebb0906c7dc956f6db8d50407b479d562a6008c
geni88 <49228790+geni88@users.noreply.github.com>
2024-02-14 17:42:46 +0900

    Update index.js
    타이틀로 변경

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index',{thisis : "reacthomepage"});
+  res.render('index',{title : "reacthomepage"});
 });
 
 // router.post('/gongsi', (req, res, next) => {
